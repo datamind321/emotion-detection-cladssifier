117e7cff475f3a6645b9d319a26cc364abbad24a
raj440singh <112772762+datamind321@users.noreply.github.com>
2023-04-25 22:46:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -240,13 +240,8 @@ def main():
 		RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
-                webrtc_ctx = webrtc_streamer(
-                             key="TEST",
-                             mode=WebRtcMode.SENDRECV,
-                             rtc_configuration=RTC_CONFIGURATION,
-                             media_stream_constraints={"video": True, "audio": False},
-                             async_processing=True,
-                              )
+               
+                              
 		webrtc_streamer(key="WYH", mode= WebRtcMode.SENDRECV,rtc_configuration=RTC_CONFIGURATION,
                                 media_stream_constraints={"video": True, "audio": False},video_processor_factory=VideoTransformer,async_processing=True,)  
 
